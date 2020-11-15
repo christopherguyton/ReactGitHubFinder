f2fec78934696909a1338684a1be8f6bbc5f3310
MrBehavior <cguyton87@gmail.com>
2020-11-15 12:28:16 -0800

    search function updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import axios from 'axios';
 import Users from './components/users/Users';
-
+import Search from './components/users/Search';
 
 class App extends Component {
   state = {
@@ -21,11 +21,18 @@ this.setState({users: res.data, loading: false})
 
     console.log(res.data);
   }
+  searchUsers = async text => {
+    this.setState({loading: true})
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({users: res.data.items, loading: false})
+  }
   render(){
     return (
       <div className='App'>
       <Navbar/>
     <div className='container'>
+    <Search searchUsers={this.searchUsers}/>
     <Users loading={this.state.loading} users={this.state.users}/>
     </div>
       </div>
